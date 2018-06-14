9df7d23abaa642c96a5686d7bb7960ee3c37668c
Chris Aquino <chris.aquino@gmail.com>
2018-06-14 09:17:06 -0400

    adds copious comments

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-const rp = require('request-promise');
+const makeRequest = require('request-promise');
 const cheerio = require('cheerio');
 const sentiment = require('node-sentiment');
+
+// The URL for the hacker news site.
 const URL = `https://news.ycombinator.com/`;
 
-// const options = {
-//   uri: URL,
-//   transform: (body) => {
-//     return cheerio.load(body);
-//   }
-// };
+// How long to wait between each request.
+// Otherwise, they know we're scraping.
+const DELAY = 700;
 
-const cheerioFy = (theURL) => {
+
+// =================================================
+// withCheerio
+// =================================================
+// a function that, given a URL,
+// returns an object that you can pass
+// to makeRequest (request-promise)
+// so that the data from the server
+// is processed through cheerio.
+const withCheerio = (theURL) => {
   return {
     uri: theURL,
     transform: (body) => {
       return cheerio.load(body);
     }
-  }
+  };
 }
 
-
+// =================================================
+// processComments
+// =================================================
+// a function that, given a URL for a
+// news.ycombinator.com comments page,
+// gets all the text for the comments,
+// and runs it through sentiment analysis
 const processComments = (theURL) => {
-  // console.log(`processing ${theURL}`);
-  rp(cheerioFy(theURL))
+  makeRequest(withCheerio(theURL))
     .then(($) => {
-    // .then((resp) => {
-      // console.log(resp);
+      // Use cheerio to find all the text from the comments
+      // and put them in an array (using .map)
+      let title = $('.storylink').text();
       let allText = $('.comment span > *').map((i, e) => {
-        // console.log($(e).text());
-        // return $(e).text();
+
+        // TODO: update the selector used above so that
+        // we don't manually have to clean up the
+        // "reply" link that appears at the end of
+        // ever comment.
         let theText = $(e).text().trim();
         if (theText !== 'reply') {
           return theText;
@@ -36,45 +53,71 @@ const processComments = (theURL) => {
           return '';
         }
       });
-      // console.log('yep');
 
-      // console.log(allText.get().join(' '));
-      return allText.get().join(' ');
+      return {
+        title,
+        // .get() needs to be used to grab
+        // the actual array.
+        commentsText: allText.get().join(' ')
+      }
+      // ^^ This is the value that is passed
+      // to the next link in the .then chain.
     })
-    .then((commentsText) => {
+    .then(({title, commentsText}) => {
+      // Use object destructuring to pull out the
+      // "vote" and "score" from the resulting
+      // sentiment analysis.
       let {vote, score} = sentiment(commentsText);
 
-      console.log(`Done processing ${theURL}:`);
+      // This is my goofy attempt at nicely printing
+      // the results.
+      console.log(`${title}:`);
       console.log(`${vote}: ${score}`);
       console.log(`---------------------`);
     })
     .catch((err) => {
-      // console.log('nope');
-      // console.log(err.message);
-      // console.log(err);
+      console.log(err.message);
     })
 }
 
-rp(cheerioFy(URL))
+// =================================================
+// This kicks off the initial HTTP request
+// =================================================
+makeRequest(withCheerio(URL))
   .then(($) => {
-    // the code will look very much
-    // like jQuery code
+    // ^^ knowing that our code will look basically
+    // like jQuery, we're using the argument name `$`
+
+    // A `Set` is designed to hold unique values.
+    // That means, there can never be duplicates.
+    // We'll use one to keep track of URLs that we've
+    // visited so we don't visit the same one twice.
     let visited = new Set([]);
+
+    // Select the story link
     $('.subtext :last-child').each( (i, element) => {
-      // console.log(element);
-      // re-cheerio the thing!
+
+      // Grab its href value
       let href = $(element).attr('href');
+
+      // If we actually have an href value...
       if (href) {
+        // and we haven't already visited it...
         if (!visited.has(href)) {
+
+          // add it to the set of visited links...
           visited.add(href);
+
+          // and schedule it to be processed.
           setTimeout(() => {
+            // The href needs to be concatenated with
+            // the main URL.
             processComments(URL + href);
-          }, 1000 * i)
+          }, DELAY * i)
         }
-        // console.log(URL + href);
       }
     });
   })
   .catch((err) => {
-    // console.log(err);
+    console.log(err.message);
   });
